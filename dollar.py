3dc9a105f6ffc2b14003acaca769b7548cadb13d
TheLinspacer <danielm2967@gmail.com>
2017-11-02 15:02:42 -0400

    Looking at manually reducing the order

--- dollar.py
+++ dollar.py
@@ -1,27 +1,36 @@
 import os
 import os.path as op
 import sys
-import json
-import pandas as pd
 import numpy as np
-from sklearn import * 
+from sklearn import linear_model
 
 from numerdd import parse, model
 
-if len(sys.argv) > 2:
-    idata = parse.getCSV()
-else:
-    idata = parse.Parser()
+#def reduceModel()
 
-idata.getData()
-modd = model.Modelerz(idata)
+if __name__ == "__main__":
 
-# Make a model with the data.
-mdl = linear_model.LogisticRegression(tol=1e-6, n_jobs=-1)
-ansnow = modd.eMod(mdl, "Logistic Regression")
-
-#Write out or not.
-g = int(input("Does the model look good enough to write out? (1 for yes 0 for no)"))
-if (g):
-    ansnow.to_csv(idata.outfile, index=False) 
+    if len(sys.argv) > 2:
+        idata = parse.getCSV()
+    else:
+        idata = parse.Parser()
+    
+    idata.getData()
+    modd = model.Modelerz(idata)
+    
+    # Make a model with the data.
+    mdl = linear_model.LogisticRegression(tol=1e-6, n_jobs=-1)
+    a1 = modd.eMod(mdl, "Logistic Regression")
+    
+    # Transformer and then transformed validation and training data
+    fd = {"mag": np.linalg.norm, "mx": np.max, "avg": np.mean, 'fft': np.max(np.fft.fft)} 
+    modd.pj.trainF = model.summit(idata.trainF, fd)
+    modd.pj.validF = model.summit(idata.validF, fd)
+    modd.pj.predF = model.summit(idata.predF, fd)
+    a2 = modd.eMod(mdl, "Reduced Logistic Regression")
+    
+    #Write out or not.
+    g = int(input("Does the model look good enough to write out? (1 for yes 0 for no)  "))
+    if (g):
+        a2.to_csv(idata.outfile, index=False) 
 
--- numerdd/model.py
+++ numerdd/model.py
@@ -14,14 +14,28 @@ def compareValid(va, vb):
     print("{:.2f} percent of the predictions are False.".format(voi*100.0))
 
 class Modelerz(object):
-    def __init__(self, prse):
-        self.pj = prse
+    def __init__(self, df):
+        self.pj = df #Should be a full parser object.  HMMM... 
 
     def eMod(self, modL, descr):
         print(descr)
-        modL.fit(self.pj.trainF, self.pj.trainT)
+        modL.fit(self.pj.trainF, self.pj.trainT.values.ravel())
         self.pre = modL.predict(self.pj.validF)
         compareValid(self.pj.validT.values.astype(int).ravel(), self.pre) # Prints validation test results.
         yprob = modL.predict_proba(self.pj.predF)
         jm = pd.DataFrame({self.pj.outvar: yprob[:,1]}, index=self.pj.predF.index)
         return jm.reset_index()
+    
+## Receives a dataframe and a dictionary of functions to apply to it.
+def summit(df, fdict):    
+    dfn = pd.DataFrame()
+    for kd in fdict.keys():
+        print(kd)
+        dfn[kd] = df.apply(fdict[kd], axis=1)
+        
+    return dfn
+    
+    
+class Transformz(object):
+    def __init__(self, prse):
+        pass # To class it or not.  Start with function above.    
